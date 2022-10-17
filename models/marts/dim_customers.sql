8771bd501d322ced9ae5981909f3d684296d0888
Tyler Norlund <tnorlund@icloud.com>
2022-10-17 21:59:21 +0000

    🔨 First build

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,24 +1,9 @@
 with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop_original.customers
-
+    select * from {{ ref('stg_customers') }}
 ),
 
 orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop_original.orders
-
+    select * from {{ ref('stg_orders') }}
 ),
 
 customer_orders as (
---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,13 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
+
+from {{ source('jaffle_shop', 'orders') }}
